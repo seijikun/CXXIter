c43ec25de4eb838c12f3f171512062b0d59d644b
seijikun <seijikun@users.noreply.github.com>
2022-01-05 19:49:28 +0000

    Deploying to gh-pages from  @ d1a3df9c6d5e70a3bcdba8f568779c66af5bb593 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -3,8 +3,8 @@ var searchData=
   ['sizehint_0',['sizeHint',['../structCXXIter_1_1IteratorTrait.html#ad07acbdbc1b8bfac76e1ce809fb60825',1,'CXXIter::IteratorTrait::sizeHint()'],['../structCXXIter_1_1SourceTrait.html#a2b357ac1506b3b70a7be8d34da3aea3a',1,'CXXIter::SourceTrait::sizeHint()'],['../classCXXIter_1_1IterApi.html#a9baddc40de1b9c63102e5c5bf886474e',1,'CXXIter::IterApi::sizeHint() const']]],
   ['skip_1',['skip',['../classCXXIter_1_1IterApi.html#ad616d50fe5b01eb2fdbfe02ced9cc2a1',1,'CXXIter::IterApi']]],
   ['skipwhile_2',['skipWhile',['../classCXXIter_1_1IterApi.html#a2a6a0f58078b8797c3ede1e6ec3896ce',1,'CXXIter::IterApi']]],
-  ['sorted_3',['sorted',['../classCXXIter_1_1IterApi.html#a25568f12125a97a9579508a5cf5e7bd6',1,'CXXIter::IterApi::sorted(TCompareFn compareFn)'],['../classCXXIter_1_1IterApi.html#a6c92644bc9fdae0fdc0795e0cfbda883',1,'CXXIter::IterApi::sorted()']]],
-  ['sortedby_4',['sortedBy',['../classCXXIter_1_1IterApi.html#a90fcf438abe534c819b846b6a3fbf8c1',1,'CXXIter::IterApi']]],
+  ['sort_3',['sort',['../classCXXIter_1_1IterApi.html#aff8dd89925c1e49327c408467ebdab22',1,'CXXIter::IterApi::sort(TCompareFn compareFn)'],['../classCXXIter_1_1IterApi.html#af8c752a71a1a7931c711ed6749c62080',1,'CXXIter::IterApi::sort()']]],
+  ['sortby_4',['sortBy',['../classCXXIter_1_1IterApi.html#afcf9678cee596627f5c9712a8efb7518',1,'CXXIter::IterApi']]],
   ['stringjoin_5',['stringJoin',['../classCXXIter_1_1IterApi.html#ab5674bb7c86728cc2520d7b3bebea437',1,'CXXIter::IterApi']]],
   ['sum_6',['sum',['../classCXXIter_1_1IterApi.html#a69c182f266a918168975a3764a1fc8eb',1,'CXXIter::IterApi']]]
 ];
