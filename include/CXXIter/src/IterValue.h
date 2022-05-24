3b227e93b8c0b399c5406fe1b20be52b20f8e74f
Markus Ebner <hiwatari.seiji@gmail.com>
2022-05-24 16:18:12 +0200

    Simplify IterValue implementation and add documentation
    - Simplified IterValue implementation by removing the concept-overload for reference and non-reference types.
      Instead implemented all of it using conditional types and clever member declarations.
    - Added proper documentation to all methods.

--- include/CXXIter/src/IterValue.h
+++ include/CXXIter/src/IterValue.h
@@ -9,85 +9,116 @@ namespace CXXIter {
 	// ITERATOR OPTIONAL (supports references)
 	// ################################################################################################
 
-	/** @private */
-	template<typename TValue>
-	class IterValue {};
-
 	/**
-	* @brief Container that is used to pass elements throught CXXIter iterator pipelines.
-	* @details This is essentially a @c std::optional<> that also supports references (in comparison
-	* to the original).
-	*/
+	 * @brief Container that is used to pass elements through CXXIter's iterator pipelines.
+	 * @details This is essentially a @c std::optional<> that also transparently supports references (in comparison
+	 * to the original). This is achieved by wrapping reference types in a @c std::reference_wrapper<> before storing
+	 * them in the internal @c std::optional<>.
+	 */
 	template<typename TValue>
-	requires (!std::is_reference_v<TValue>)
-	class IterValue<TValue> {
-		std::optional<TValue> inner;
-	public:
-		IterValue() {}
-		IterValue(const TValue& value) : inner(value) {}
-		IterValue(TValue&& value) : inner(std::forward<TValue>(value)) {}
-		IterValue<TValue>& operator=(IterValue<TValue>&& o) = default;
-		IterValue(IterValue<TValue>&& o) : inner(std::move(o.inner)) {
-			o.inner.reset();
-		};
-
-		inline const TValue& value() const { return inner.value(); }
-		inline TValue& value() { return inner.value(); }
-		inline const TValue& value(TValue&& def) const { return inner.value_or(def); }
-		inline TValue& value(TValue&& def) { return inner.value_or(def); }
-
-		bool has_value() const { return inner.has_value(); }
-		std::optional<TValue> toStdOptional() {
-			return std::optional<TValue>(std::move(inner));
-		}
-		operator std::optional<TValue>() { return toStdOptional(); }
-
-		IterValue<TValue>& operator=(TValue&& o) {
-			inner = std::forward<TValue>(o);
-			return *this;
-		}
-
-		template<typename TOutValue, std::invocable<TValue&&> TMapFn>
-		IterValue<TOutValue> map(TMapFn mapFn) {
-			if(!has_value()) { return {}; }
-			return mapFn(std::forward<TValue>(value()));
-		}
-	};
-
-	template<typename TValue>
-	requires std::is_reference_v<TValue>
-	class IterValue<TValue> {
+	class IterValue {
+	private:
+		/** Owned (references removed) version of the type that is to be stored in this IterValue. */
 		using TValueDeref = std::remove_reference_t<TValue>;
-		std::optional<std::reference_wrapper<TValueDeref>> inner;
+		using TValueStore = std::conditional_t<
+			std::is_reference_v<TValue>,
+			std::reference_wrapper<TValueDeref>,
+			TValue
+		>;
+
+		std::optional<TValueStore> inner;
+
 	public:
-		IterValue() {}
-		IterValue(TValue value) : inner(value) {}
-		IterValue<TValue>& operator=(IterValue<TValue>&& o) = default;
-		IterValue(IterValue<TValue>&& o) : inner(std::move(o.inner)) {
+		/** ctor */
+		IterValue() noexcept {}
+		/** ctor */
+		IterValue(TValue value) noexcept requires std::is_reference_v<TValue> : inner(value) {}
+		/** ctor */
+		IterValue(const TValueDeref& value) noexcept requires (!std::is_reference_v<TValue>) : inner(value) {}
+		/** ctor */
+		IterValue(TValueDeref&& value) noexcept : inner(std::forward<TValueDeref>(value)) {}
+
+		/** Assignment from another IterValue instance. */
+		IterValue& operator=(IterValue&& o) = default;
+		/** Assignment from another IterValue instance. */
+		IterValue(IterValue&& o) : inner(std::move(o.inner)) {
 			o.inner.reset();
 		};
 
-		inline const TValue& value() const { return inner.value(); }
-		inline TValue& value() { return inner.value(); }
-		inline const TValue& value(TValue&& def) const { return inner.value_or(def); }
-		inline TValue& value(TValue&& def) { return inner.value_or(def); }
-
-		bool has_value() const { return inner.has_value(); }
-		std::optional<TValueDeref> toStdOptional() {
-			if(inner.has_value()) { return inner.value(); }
-			return {};
-		}
-		operator std::optional<TValueDeref>() { return toStdOptional(); }
-
-		IterValue<TValue>& operator=(TValue&& o) {
-			inner = std::forward<TValue>(o);
+		/** Assignment from an instance of the stored type. */
+		auto& operator=(TValueDeref&& o) {
+			this->inner = std::forward<decltype(o)>(o);
 			return *this;
 		}
 
-		template<typename TOutValue, std::invocable<TValue> TMapFn>
+		/**
+		 * @brief Get the contained value (if any).
+		 * @throws If this is called when no value is contained.
+		 * @return const reference to the contained value.
+		 */
+		inline const TValueDeref& value() const { return inner.value(); }
+		/**
+		 * @brief Get the contained value (if any).
+		 * @throws If this is called when no value is contained.
+		 * @return reference to the contained value.
+		 */
+		inline TValueDeref& value() { return inner.value(); }
+		/**
+		 * @brief Get the contained value, or alternatively the given @p def if none is present.
+		 * @param def Default value to return when this optional does not contain a value.
+		 * @return const reference to the contained value (if any), or alternatively the given @p def value.
+		 */
+		inline const TValueDeref& value_or(TValueDeref&& def) const noexcept { return inner.value_or(def); }
+		/**
+		 * @brief Get the contained value, or alternatively the given @p def if none is present.
+		 * @param def Default value to return when this optional does not contain a value.
+		 * @return reference to the contained value (if any), or alternatively the given @p def value.
+		 */
+		inline TValueDeref& value_or(TValueDeref&& def) noexcept { return inner.value_or(def); }
+
+		/**
+		 * @brief Get whether this optional IteratorValue contains a value.
+		 * @return @c true when this IterValue contains a value, @c false otherwise.
+		 */
+		bool has_value() const noexcept { return this->inner.has_value(); }
+
+		/**
+		 * @brief Convert this IterValue to a @c std::optional<> on the owned (no-reference) type.
+		 * @details If the contained value is a reference, the value will be copied into the returned @c std::optional<>.
+		 * @return @c std::optional<> containing an owned version of the value contained by this IterValue.
+		 */
+		std::optional<TValueStore> toStdOptional() noexcept {
+			return std::optional<TValueStore>(std::move(this->inner));
+		}
+		/**
+		 * @brief Cast to @c std::optional<>.
+		 */
+		operator std::optional<TValueStore>() noexcept { return toStdOptional(); }
+
+		/**
+		 * @brief Map this IterValue's contained value (if any) to the requested new @p TOutValue type, using the given @p mapFn
+		 * @tparam The requested output type for the mapping operation.
+		 * @param mapFn Mapper function that takes this IterValue's contained value (if any) to map it to an instance of @p TOutValue
+		 * @return A new IterValue containing the mapped value if this instance contained a value.
+		 */
+		template<typename TOutValue, std::invocable<TValueDeref&&> TMapFn>
+		requires (!std::is_reference_v<TValue>)
 		IterValue<TOutValue> map(TMapFn mapFn) {
 			if(!has_value()) { return {}; }
-			return mapFn(value());
+			return mapFn(std::forward<TValueDeref>(value()));
+		}
+
+		/**
+		 * @brief Map this IterValue's contained value (if any) to the requested new @p TOutValue type, using the given @p mapFn
+		 * @tparam The requested output type for the mapping operation.
+		 * @param mapFn Mapper function that takes this IterValue's contained value (if any) to map it to an instance of @p TOutValue
+		 * @return A new IterValue containing the mapped value if this instance contained a value.
+		 */
+		template<typename TOutValue, std::invocable<TValue> TMapFn>
+		requires std::is_reference_v<TValue>
+		IterValue<TOutValue> map(TMapFn mapFn) {
+			if(!this->has_value()) { return {}; }
+			return mapFn(this->value());
 		}
 	};
 
