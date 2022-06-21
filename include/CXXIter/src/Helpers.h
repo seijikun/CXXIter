9bfbf22201bad7b153ecebb8744f20964179f6f4
Markus Ebner <hiwatari.seiji@gmail.com>
2022-06-21 15:28:17 +0200

    Refactor helpers into fn namespace, add filterIsOneOf helper
    - Refactored helpers into their own fn namespace for easier discovery.
    - Added filterIsOneOf helpers that allow filtering the iterator elements
      against a list of accepted values similar to mysql's IN(...,...)
      operator.

--- include/CXXIter/src/Helpers.h
+++ include/CXXIter/src/Helpers.h
@@ -3,16 +3,16 @@
 #include <tuple>
 #include <cstdlib>
 #include <optional>
-
-namespace CXXIter {
+#include <unordered_set>
 
 /**
- * @name Helpers
- * These functions are small helper functions that can be used in conjunction with the
+ * @brief Namespace that contains helper functions providing commonly required functionality
+ * when working with CXXIter.
+ * @details These functions are small helper functions that can be used in conjunction with the
  * iterator API interface. For example by constructing lambdas for reusable tasks
  * with a clear and concise API, instead of copying the lambda code itself everywhere it is needed.
  */
-//@{
+namespace CXXIter::fn {
 
 	// ################################################################################################
 	// HELPERS
@@ -32,7 +32,7 @@ namespace CXXIter {
 	 *		.indexed()
 	 *		.sortBy<CXXIter::DESCENDING>(CXXIter::unzip<1>())
 	 *		.collect<std::vector>();
-	 *  // output = {{3, 3.0f}, {1, 2.0f}, {0, 1.0f}, {2, 0.5f}, {4, -42.0f}}
+	 *  // output == {{3, 3.0f}, {1, 2.0f}, {0, 1.0f}, {2, 0.5f}, {4, -42.0f}}
 	 * @endcode
 	 */
 	template<size_t FIELD_IDX>
@@ -41,7 +41,7 @@ namespace CXXIter {
 	}
 
 	/**
-	 * @brief Helper to construct a filterMap lambda that takes a pointer, dyncasts it to the requested type and
+	 * @brief Helper to construct a filterMap() lambda that takes a pointer, dyncasts it to the requested type and
 	 * returns a @c std::optional<TDynCastTarget> with the result if the cast was successfull.
 	 * @details You can use this shortcut everywhere, where a lambda is required that takes an element and
 	 * returns an @c std::optional<TDynCastTarget> that is set
@@ -66,9 +66,9 @@ namespace CXXIter {
 	 *  std::vector<Child1*> output = CXXIter::from(input)
 	 *  		.filterMap(CXXIter::tryDynCast<Child1*>())
 	 *  		.collect<std::vector>();
-	 *  ASSERT_EQ(output.size(), 2);
-	 *  ASSERT_EQ(output[0]->id, "0");
-	 *  ASSERT_EQ(output[1]->id, "1");
+	 *  // output.size() == 2
+	 *  // output[0]->id == "0"
+	 *  // output[1]->id == "1"
 	 *  CXXIter::from(output).forEach([](auto ptr) { delete ptr; });
 	 * @endcode
 	 */
@@ -81,6 +81,81 @@ namespace CXXIter {
 		};
 	}
 
-//}@
+	/**
+	 * @brief Helper that constructs a filter() lambda that filters the iterator elements by calling
+	 * the given @p filterExtractFn on each element and then checking whether that is contained in the given
+	 * list of @p acceptedValues.
+	 * @param filterExtractFn Lambda that extracts a value for each iterator element, which is then searched
+	 * for in the list of @p acceptedValues.
+	 * @param acceptedValues List of comparison-values that are allowed to remain in the iterator.
+	 * @return Lambda that can be passed to CXXIter::filter to filter the iterator elements with a given
+	 * list of @p acceptedValues.
+	 *
+	 * Usage Example:
+	 * @code
+	 *  enum class CakeType { Sacher, ButterCake, CheeseCake, ChocolateCake, StrawberryCake };
+	 *  struct Cake {
+	 *  	CakeType type;
+	 *  	float volume;
+	 *  	bool operator==(const Cake& o) const { return o.type == type && o.volume == volume; }
+	 *  };
+	 *
+	 *  std::vector<Cake> input = {
+	 *  	{ CakeType::Sacher, 1.33f }, { CakeType::CheeseCake, 5.0f },
+	 *  	{ CakeType::ButterCake, 2.33f }, { CakeType::Sacher, 42.0f },
+	 *  	{ CakeType::StrawberryCake, 1.6f }, { CakeType::ChocolateCake, 55.0f },
+	 *  	{ CakeType::Sacher, 3.63f }, { CakeType::StrawberryCake, 14.0f }
+	 *  };
+	 *  std::vector<Cake> output = CXXIter::from(input)
+	 *  	.filter(CXXIter::fn::filterIsOneOf(
+	 *  		[](const Cake& cake) { return cake.type; },
+	 *  		{CakeType::Sacher, CakeType::ChocolateCake}
+	 *  	))
+	 *  	.collect<std::vector>();
+	 *  // output == { Cake {CakeType::Sacher, 1.33f}, Cake {CakeType::Sacher, 42.0f},
+	 *					Cake {CakeType::ChocolateCake, 55.0f}, Cake {CakeType::Sacher, 3.63f} };
+	 * @endcode
+	 */
+	template<typename TItem, typename TFilterExtractFn>
+	auto filterIsOneOf(TFilterExtractFn filterExtractFn, const std::initializer_list<TItem>& acceptedValues) {
+		std::unordered_set<TItem> _acceptedValues = acceptedValues;
+		return [_acceptedValues = std::move(_acceptedValues), filterExtractFn](const auto& item) {
+			return _acceptedValues.contains(filterExtractFn(item));
+		};
+	}
+
+	/**
+	 * @brief Helper that constructs a filter() lambda that filters an iterator's elements by checking whether
+	 * they are contained in the given list of @p acceptedValues.
+	 * @param acceptedValues List of values that are allowed to remain in the iterator.
+	 * @return Lambda that can be passed to CXXIter::filter to filter the iterator elements with a given
+	 * list of @p acceptedValues.
+	 *
+	 * Usage Example:
+	 * @code
+	 *  enum class CakeType { Sacher, ButterCake, CheeseCake, ChocolateCake, StrawberryCake };
+	 *  struct Cake {
+	 *  	CakeType type;
+	 *  	float volume;
+	 *  	bool operator==(const Cake& o) const { return o.type == type && o.volume == volume; }
+	 *  };
+	 *
+	 *  std::vector<Cake> input = {
+	 *  	{ CakeType::Sacher, 1.33f }, { CakeType::CheeseCake, 5.0f },
+	 *  	{ CakeType::ButterCake, 2.33f }, { CakeType::Sacher, 42.0f },
+	 *  	{ CakeType::StrawberryCake, 1.6f }, { CakeType::ChocolateCake, 55.0f },
+	 *  	{ CakeType::Sacher, 3.63f }, { CakeType::StrawberryCake, 14.0f }
+	 *  };
+	 *  std::vector<CakeType> output = CXXIter::from(input)
+	 *  		.map([](const Cake& cake) { return cake.type; })
+	 *  		.filter(CXXIter::fn::filterIsOneOf({CakeType::Sacher, CakeType::ChocolateCake}))
+	 *  		.collect<std::vector>();
+	 *  // output == {CakeType::Sacher, CakeType::Sacher, CakeType::ChocolateCake, CakeType::Sacher};
+	 * @endcode
+	 */
+	template<typename TItem>
+	auto filterIsOneOf(const std::initializer_list<TItem>& acceptedValues) {
+		return filterIsOneOf<TItem>([](const auto& item) { return item; }, acceptedValues);
+	}
 
 }
--- tests/TestHelpers.cpp
+++ tests/TestHelpers.cpp
@@ -9,7 +9,7 @@ TEST(CXXIter, unzip) {
 	std::vector<float> input = {1.0f, 2.0f, 0.5f, 3.0f, -42.0f};
 	std::vector<std::pair<size_t, float>> output = CXXIter::from(input).copied()
 		.indexed()
-		.sortBy<CXXIter::DESCENDING>(CXXIter::unzip<1>())
+		.sortBy<CXXIter::DESCENDING>(CXXIter::fn::unzip<1>())
 		.collect<std::vector>();
 	ASSERT_EQ(input.size(), output.size());
 	ASSERT_THAT(output, ElementsAre(Pair(3, 3.0f), Pair(1, 2.0f), Pair(0, 1.0f), Pair(2, 0.5f), Pair(4, -42.0f)));
@@ -28,7 +28,7 @@ TEST(CXXIter, tryDynCast) {
 	{ // normal
 		std::vector<Parent*> input = { new Parent(), new Child1("0"), new Child1("1"), new Child2() };
 		std::vector<Child1*> output = CXXIter::from(input)
-				.filterMap(CXXIter::tryDynCast<Child1*>())
+				.filterMap(CXXIter::fn::tryDynCast<Child1*>())
 				.collect<std::vector>();
 		ASSERT_EQ(output.size(), 2);
 		ASSERT_EQ(output[0]->id, "0");
@@ -38,7 +38,7 @@ TEST(CXXIter, tryDynCast) {
 	{ // const
 		std::vector<const Parent*> input = { new Parent(), new Child1("0"), new Child1("1"), new Child2() };
 		std::vector<const Child1*> output = CXXIter::from(input)
-				.filterMap(CXXIter::tryDynCast<const Child1*>())
+				.filterMap(CXXIter::fn::tryDynCast<const Child1*>())
 				.collect<std::vector>();
 		ASSERT_EQ(output.size(), 2);
 		ASSERT_EQ(output[0]->id, "0");
@@ -48,11 +48,46 @@ TEST(CXXIter, tryDynCast) {
 	{ // volatile
 		std::vector<volatile Parent*> input = { new Parent(), new Child1("0"), new Child1("1"), new Child2() };
 		std::vector<volatile Child1*> output = CXXIter::from(input)
-				.filterMap(CXXIter::tryDynCast<volatile Child1*>())
+				.filterMap(CXXIter::fn::tryDynCast<volatile Child1*>())
 				.collect<std::vector>();
 		ASSERT_EQ(output.size(), 2);
 		ASSERT_EQ(const_cast<std::string&>(output[0]->id), "0");
 		ASSERT_EQ(const_cast<std::string&>(output[1]->id), "1");
 		CXXIter::from(output).forEach([](auto ptr) { delete ptr; });
+	}
+}
+
+TEST(CXXIter, filterIsOneOf) {
+	enum class CakeType { Sacher, ButterCake, CheeseCake, ChocolateCake, StrawberryCake };
+	struct Cake {
+		CakeType type;
+		float volume;
+		bool operator==(const Cake& o) const { return o.type == type && o.volume == volume; }
+	};
+
+	std::vector<Cake> input = {
+		{ CakeType::Sacher, 1.33f }, { CakeType::CheeseCake, 5.0f },
+		{ CakeType::ButterCake, 2.33f }, { CakeType::Sacher, 42.0f },
+		{ CakeType::StrawberryCake, 1.6f }, { CakeType::ChocolateCake, 55.0f },
+		{ CakeType::Sacher, 3.63f }, { CakeType::StrawberryCake, 14.0f }
+	};
+	{
+		std::vector<Cake> output = CXXIter::from(input)
+				.filter(CXXIter::fn::filterIsOneOf(
+					[](const Cake& cake) { return cake.type; },
+					{CakeType::Sacher, CakeType::ChocolateCake}
+				))
+				.collect<std::vector>();
+		ASSERT_EQ(output.size(), 4);
+		ASSERT_THAT(output, ElementsAre( Cake {CakeType::Sacher, 1.33f}, Cake {CakeType::Sacher, 42.0f},
+										 Cake {CakeType::ChocolateCake, 55.0f}, Cake {CakeType::Sacher, 3.63f} ));
+	}
+	{
+		std::vector<CakeType> output = CXXIter::from(input)
+				.map([](const Cake& cake) { return cake.type; })
+				.filter(CXXIter::fn::filterIsOneOf({CakeType::Sacher, CakeType::ChocolateCake}))
+				.collect<std::vector>();
+		ASSERT_EQ(output.size(), 4);
+		ASSERT_THAT(output, ElementsAre( CakeType::Sacher, CakeType::Sacher, CakeType::ChocolateCake, CakeType::Sacher ));
 	}
 }
